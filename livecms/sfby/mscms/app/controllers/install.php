58cf44d52a5adb520b8756f74ca2ed1e0c2784c4
mymacbook <1017962047@qq.com>
2018-08-25 14:09:25 +0800

    livecms

--- livecms/sfby/mscms/app/controllers/install.php
+++ livecms/sfby/mscms/app/controllers/install.php
@@ -174,18 +174,17 @@ class Install extends Mscms_Controller {
                 //$lnk=@mysql_connect($dbhost,$dbuser,$dbpwd);
                 $lnk=@mysqli_connect($dbhost,$dbuser,$dbpwd);
 
-                print_r(mysqli_select_db($lnk,$dbname));
-                exit();
+
 
                 if(!$lnk) {
                          exit('2');
                 }else{
                    if(!mysqli_select_db($lnk,$dbname)){
-                        if(!@mysql_query("CREATE DATABASE `".$dbname."`")){
+                        if(!@mysqli_query($lnk,"CREATE DATABASE `".$dbname."`")){
                              exit('3');
                         }
                    }
-				   if(mysql_select_db($dbname)){
+				   if(mysqli_select_db($lnk,$dbname)){
 					    //ÐÞ¸ÄÊý¾Ý¿âÅäÖÃ
                         $this->load->helper('string');
                         $CS_Encryption_Key='mscms_'.random_string('alnum',10);
@@ -201,8 +200,8 @@ class Install extends Mscms_Controller {
 	                    if(!write_file('./mscms/lib/Cs_DB.php', $config)) exit('5');
 
 		                $tables = array();
-		                $query = mysql_query("SHOW TABLES FROM `".$dbname."`");
-		                while($r = mysql_fetch_row($query)) {
+		                $query = mysqli_query($lnk,"SHOW TABLES FROM `".$dbname."`");
+		                while($r = mysqli_fetch_row($lnk,$query)) {
 			                $tables[] = $r[0];
 		                }
 		                if($tables && in_array($dbprefix.'plugins', $tables)) {
