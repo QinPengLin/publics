13950fee893ebb15ead90c6ce099d3994f3dd6fb
mymacbook <1017962047@qq.com>
2018-08-27 17:09:12 +0800

    znz

--- znz/install_package/phpcms/libs/functions/global.func.php
+++ znz/install_package/phpcms/libs/functions/global.func.php
@@ -745,7 +745,7 @@ function pages($num, $curr_page, $perpage = 20, $urlrule = '', $array = array(),
 		if($curr_page>0) {
 			$multipage .= ' <a href="'.pageurl($urlrule, $curr_page-1, $array).'" class="a1">'.L('previous').'</a>';
 			if($curr_page==1) {
-				$multipage .= ' <span>1</span>';
+				$multipage .= ' <a href="#" class="Ahover">1</a>';
 			} elseif($curr_page>6 && $more) {
 				$multipage .= ' <a href="'.pageurl($urlrule, 1, $array).'">1</a>..';
 			} else {
@@ -756,7 +756,7 @@ function pages($num, $curr_page, $perpage = 20, $urlrule = '', $array = array(),
 			if($i != $curr_page) {
 				$multipage .= ' <a href="'.pageurl($urlrule, $i, $array).'">'.$i.'</a>';
 			} else {
-				$multipage .= ' <span>'.$i.'</span>';
+				$multipage .= ' <a href="#" class="Ahover">'.$i.'</a>>';
 			}
 		}
 		if($curr_page<$pages) {
@@ -766,7 +766,7 @@ function pages($num, $curr_page, $perpage = 20, $urlrule = '', $array = array(),
 				$multipage .= ' <a href="'.pageurl($urlrule, $pages, $array).'">'.$pages.'</a> <a href="'.pageurl($urlrule, $curr_page+1, $array).'" class="a1">'.L('next').'</a>';
 			}
 		} elseif($curr_page==$pages) {
-			$multipage .= ' <span>'.$pages.'</span> <a href="'.pageurl($urlrule, $curr_page, $array).'" class="a1">'.L('next').'</a>';
+			$multipage .= ' <a href="#" class="Ahover">'.$pages.'</a> <a href="'.pageurl($urlrule, $curr_page, $array).'" class="a1">'.L('next').'</a>';
 		} else {
 			$multipage .= ' <a href="'.pageurl($urlrule, $pages, $array).'">'.$pages.'</a> <a href="'.pageurl($urlrule, $curr_page+1, $array).'" class="a1">'.L('next').'</a>';
 		}
