0590c7a8dafa950d59d0b707b6892df2612740f0
mymacbook <1017962047@qq.com>
2018-08-29 00:52:15 +0800

    livecms

--- livecms/sfby/mscms/app/core/Mscms_Controller.php
+++ livecms/sfby/mscms/app/core/Mscms_Controller.php
@@ -4,8 +4,9 @@
 // --------------------------------------------------
 class Mscms_Controller extends CI_Controller {
 	function __construct() {
-        exit('lalal88666');
+
 		parent::__construct();
+        exit('lalal88666');
 		header('X-Generator: '.cs_base64_decode('Q3NjbXMgdjQgKGh0dHA6Ly93d3cuY2hzaGNtcy5jb20p'));
 	}
 }
