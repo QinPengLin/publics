4b7fa85ad7575bb28bb2d58e85c57e89eead1f65
mymacbook <1017962047@qq.com>
2018-09-07 13:52:10 +0800

    hd

---
+++ admin_hd/install_package/api/add_content.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macbook
+ * Date: 2018/9/7
+ * Time: 下午1:49
+ */
+defined('IN_PHPCMS') or exit('No permission resources.');
+$db = pc_base::load_model('content_model');
+print_r($db);
