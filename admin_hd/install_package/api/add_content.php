0b1808d2e8f7e5a11504ae63aca86f508c5dbb19
mymacbook <1017962047@qq.com>
2018-09-07 14:29:40 +0800

    hd

--- admin_hd/install_package/api/add_content.php
+++ admin_hd/install_package/api/add_content.php
@@ -8,12 +8,15 @@
 defined('IN_PHPCMS') or exit('No permission resources.');
 $db = pc_base::load_model('content_model');
 $categorys = getcache('category_content_'.get_siteid(),'commons');
+$post=$_POST;
+print_r($post);
+echo '</br>';
 print_r('no');
-if(isset($_POST['dosubmit'])) {
+if(isset($post['dosubmit'])) {
     print_r('yes');
     define('INDEX_HTML', true);
-    $catid = $_POST['info']['catid'] = intval($_POST['info']['catid']);
-    if (trim($_POST['info']['title']) == '') showmessage(L('title_is_empty'));
+    $catid = $post['info']['catid'] = intval($post['info']['catid']);
+    if (trim($post['info']['title']) == '') showmessage(L('title_is_empty'));
     $category = $categorys[$catid];
     print_r($category);
     if($category['type']==0) {
@@ -22,11 +25,11 @@ if(isset($_POST['dosubmit'])) {
         $setting = string2array($category['setting']);
         $workflowid = $setting['workflowid'];
         if($workflowid && $_POST['status']!=99) {
-            $_POST['info']['status'] =  1;
+            $post['info']['status'] =  1;
         }else{
-            $_POST['info']['status'] = 99;
+            $post['info']['status'] = 99;
         }
-        $db->add_content($_POST['info']);
+        $db->add_content($post['info']);
         showmessage(L('add_success').L('2s_close'),'blank','','','function set_time() {$("#secondid").html(1);}setTimeout("set_time()", 500);setTimeout("window.close()", 1200);');
     }
 }
