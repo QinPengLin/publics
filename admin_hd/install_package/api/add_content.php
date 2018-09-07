b43a1a45c0def6cb0b1c9ddd66beed7fc79cd25c
mymacbook <1017962047@qq.com>
2018-09-07 14:57:10 +0800

    hd

--- admin_hd/install_package/api/add_content.php
+++ admin_hd/install_package/api/add_content.php
@@ -6,7 +6,7 @@
  * Time: 下午1:49
  */
 defined('IN_PHPCMS') or exit('No permission resources.');
-print_r(getallheaders());
+print_r($_SERVER);
 exit();
 $db = pc_base::load_model('content_model');
 $categorys = getcache('category_content_'.get_siteid(),'commons');
