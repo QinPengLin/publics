c38f2ae92eb6ee3ae9168149414947c47bb677c6
mymacbook <1017962047@qq.com>
2018-09-07 14:14:04 +0800

    hd

--- admin_hd/install_package/api/add_content.php
+++ admin_hd/install_package/api/add_content.php
@@ -8,4 +8,22 @@
 defined('IN_PHPCMS') or exit('No permission resources.');
 $db = pc_base::load_model('content_model');
 $categorys = getcache('category_content_'.get_siteid(),'commons');
-print_r($categorys);
+if(isset($_POST['dosubmit'])) {
+    define('INDEX_HTML', true);
+    $catid = $_POST['info']['catid'] = intval($_POST['info']['catid']);
+    if (trim($_POST['info']['title']) == '') showmessage(L('title_is_empty'));
+    $category = $categorys[$catid];
+    if($category['type']==0) {
+        $modelid = $categorys[$catid]['modelid'];
+        $db->set_model($modelid);
+        $setting = string2array($category['setting']);
+        $workflowid = $setting['workflowid'];
+        if($workflowid && $_POST['status']!=99) {
+            $_POST['info']['status'] =  1;
+        }else{
+            $_POST['info']['status'] = 99;
+        }
+        $db->add_content($_POST['info']);
+        showmessage(L('add_success').L('2s_close'),'blank','','','function set_time() {$("#secondid").html(1);}setTimeout("set_time()", 500);setTimeout("window.close()", 1200);');
+    }
+}
