239a32d158e66f5250950f53f04363a6aefc8ae6
mymacbook <1017962047@qq.com>
2018-10-17 14:24:35 +0800

    hd

--- admin_hd/install_package/api/notify.php
+++ admin_hd/install_package/api/notify.php
@@ -5,6 +5,11 @@
  * Date: 2018/10/16
  * Time: 上午10:37
  */
+define('PHPCMS_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+print_r(PHPCMS_PATH);
+    exit;
+include PHPCMS_PATH.'phpcms/base.php';
+$param = pc_base::load_sys_class('param');
 pc_base::load_app_class('respond', 'pay', 0);
 $respond = new respond();//respond_iiipi
 $respond->respond_iiipi();
