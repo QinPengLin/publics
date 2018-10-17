7395cd6255d01a0c3b63e81748966b5ed93ed2b9
mymacbook <1017962047@qq.com>
2018-10-17 14:28:21 +0800

    hd

--- admin_hd/install_package/api/notify.php
+++ admin_hd/install_package/api/notify.php
@@ -6,8 +6,6 @@
  * Time: 上午10:37
  */
 define('PHPCMS_PATH', str_replace("api/","phpcms/",dirname(__FILE__).DIRECTORY_SEPARATOR));
-print_r(PHPCMS_PATH);
-    exit;
 include PHPCMS_PATH.'base.php';
 $param = pc_base::load_sys_class('param');
 pc_base::load_app_class('respond', 'pay', 0);
