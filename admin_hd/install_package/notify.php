4a502d265c94f92020ef4e7a6446e2ed484aafde
mymacbook <1017962047@qq.com>
2018-10-17 14:38:39 +0800

    hd

--- admin_hd/install_package/notify.php
+++ admin_hd/install_package/notify.php
@@ -8,6 +8,8 @@
 define('PHPCMS_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 include PHPCMS_PATH.'phpcms/base.php';
 pc_base::load_app_class('respond', 'pay', 0);
-exit();
+
 $respond = new respond();//respond_iiipi
+print_r($respond);
+exit();
 $respond->respond_iiipi();
