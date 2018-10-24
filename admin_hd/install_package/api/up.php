35e850d12f2107e00e1cbe94b96573cdd862d4cf
mymacbook <1017962047@qq.com>
2018-10-24 12:17:09 +0800

    up

--- admin_hd/install_package/api/up.php
+++ admin_hd/install_package/api/up.php
@@ -5,12 +5,13 @@
  * Date: 2018/10/24
  * Time: 下午12:10
  */
-echo 'ye';
-exit;
+
 pc_base::load_sys_class('attachment','',0);
 $module = 'content';
 $catid = 17;
 $siteid = get_siteid();
+echo $siteid;
+exit;
 $site_setting = get_site_setting($siteid);
 $site_allowext = $site_setting['upload_allowext'];
 print_r($site_allowext);
