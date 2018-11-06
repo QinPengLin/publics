2f12e56dcdb93429c8956ad07d7718422d07c34c
mymacbook <1017962047@qq.com>
2018-11-07 02:25:17 +0800

    会员自助升级mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -7,8 +7,10 @@
  */
 $m = new MongoDB\Driver\Manager("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
 
-//$db = $m->porn;
-//$porns = $db->porns;
-//$data=$porns->findOne();
-
-print_r($m->getServers ());
+$id      = new \MongoDB\BSON\ObjectId("5be1b8bbdb183d073806d527");
+$filter  = ['_id' => $id];
+$options = [];
+$query   = new \MongoDB\Driver\Query($filter, $options);
+$rows    = $m->executeQuery('porn.porns', $query)->toArray();
+echo '<pre>';print_r($rows);echo '</pre>';exit;
+﻿
