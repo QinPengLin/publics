abaf6c856b2d0906f9ca4909f9ec1bec02895be0
mymacbook <1017962047@qq.com>
2018-11-07 02:03:44 +0800

    会员自助升级mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -6,23 +6,9 @@
  * Time: 下午11:38
  */
 $m = new MongoDB\Driver\Manager("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
-$db = 'porn';
-function exec($opts,$m,$db) {
-    $cmd = new MongoDB\Driver\Command($opts);
-    $res =  $m->executeCommand($db, $cmd);
-    return $res->toArray();
-}
-
-$cmd = [
-    'find' => 'porns', // collection表名
-    'projection' => ['remoteId' => 32122919]
-];
-
-$res = exec($cmd,$m,$db);
-
 
 //$db = $m->porn;
 //$porns = $db->porns;
 //$data=$porns->findOne();
 
-print_r($res);
+print_r($m);
