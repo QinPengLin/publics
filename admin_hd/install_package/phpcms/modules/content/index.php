4d3cf08eb3f1e2491b19e7e54f92c5e615341292
mymacbook <1017962047@qq.com>
2018-11-06 16:49:11 +0800

    测试

--- admin_hd/install_package/phpcms/modules/content/index.php
+++ admin_hd/install_package/phpcms/modules/content/index.php
@@ -47,9 +47,7 @@ class index {
                 $forward = urlencode(get_url());
                 showmessage(L('login_website'),APP_PATH.'index.php?m=member&c=index&a=login&forward='.$forward);
             }
-            echo $_groupid;
-            exit();
-            if(!in_array($_groupid,[4,5,6])) showmessage(L('no_priv'));
+            if(!in_array($_groupid,[2,3,5])) showmessage(L('no_priv'));
             $CAT = $CATEGORYS[$catid];
             $template = $template ? $template : $CAT['setting']['show_template'];
             if(!$template) $template = 'show';
