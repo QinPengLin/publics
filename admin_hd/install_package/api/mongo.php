525a33d41daa3625fc5f6f9e5f6a7070787b4d31
mymacbook <1017962047@qq.com>
2018-11-06 23:56:28 +0800

    mong

---
+++ admin_hd/install_package/api/mongo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macbook
+ * Date: 2018/11/4
+ * Time: 下午11:38
+ */
+$m = new MongoClient("mongodb://149.28.122.121:27017/mongouser:Asdfgh123456");
+print_r($m);
