a14f0830490465a48ad0428969c1de5a8a8de339
mymacbook <1017962047@qq.com>
2018-11-07 01:14:38 +0800

    会员自助升级mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -5,7 +5,7 @@
  * Date: 2018/11/4
  * Time: 下午11:38
  */
-$m = new Mongo("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
+$m = new MongoClient("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
 //$db = $m->porn;
 //$porns = $db->porns;
 //$data=$porns->findOne();
