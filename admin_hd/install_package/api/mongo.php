92077417bfd45f70548ce5a81ebcb3ff318540b0
mymacbook <1017962047@qq.com>
2018-11-06 23:58:10 +0800

    mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -5,5 +5,6 @@
  * Date: 2018/11/4
  * Time: 下午11:38
  */
-$m = new MongoClient("mongodb://149.28.122.121:27017/mongouser:Asdfgh123456");
-print_r($m);
+echo 'yes';
+//$m = new MongoClient("mongodb://149.28.122.121:27017/mongouser:Asdfgh123456");
+//print_r($m);
