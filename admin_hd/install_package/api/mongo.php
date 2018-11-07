123876b766c9cf205cc9a2612bd051468d02529b
mymacbook <1017962047@qq.com>
2018-11-07 12:10:05 +0800

    会员自助升级mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -6,51 +6,15 @@
  * Time: 下午11:38
  */
 
-class  CreateCollection  {
-    protected  $cmd  = array();
-
-    function  __construct ( $collectionName ) {
-        $this -> cmd [ "aggregate" ] = (string) $collectionName ;
-    }
-    function  setAutoIndexId ( $bool ) {
-        $this -> cmd [ "autoIndexId" ] = (bool) $bool ;
-    }
-    function  setCappedCollection ( $maxBytes ,  $maxDocuments  =  false ) {
-        $this -> cmd [ "capped" ] =  true ;
-        $this -> cmd [ "size" ]   = (int) $maxBytes ;
-
-        if ( $maxDocuments ) {
-            $this -> cmd [ "max" ] = (int) $maxDocuments ;
-        }
-    }
-    function  usePowerOf2Sizes ( $bool ) {
-        if ( $bool ) {
-            $this -> cmd [ "flags" ] =  1 ;
-        } else {
-            $this -> cmd [ "flags" ] =  0 ;
-        }
-    }
-    function  setFlags ( $flags ) {
-        $this -> cmd [ "flags" ] = (int) $flags ;
-    }
-    function  getCommand () {
-        return new  MongoDB\Driver\Command( $this -> cmd );
-    }
-    function  getCollectionName () {
-        return  $this -> cmd [ "create" ];
-    }
-}
 
 
 $m = new MongoDB\Driver\Manager("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
 
-$createCollection  = new  CreateCollection ( "porns" );
-$createCollection -> setCappedCollection ( 64  *  1024 );
+$command = new \MongoDB\Driver\Command(['count' => '_id','query'=>'_id','aggregate' => 'porns']);
 
-$command  =  $createCollection -> getCommand ();
-$cursor  =  $m-> executeCommand ( "porn" ,  $command );
-$response  =  $cursor -> toArray ()[ 0 ];
-var_dump ( $response );
+$result = $m->executeCommand('porn',$command);
+
+var_dump ($result);
 
 
 //$filter  = [];
