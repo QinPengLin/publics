cfa20b49749b34a374ef9a83ca393311f89e9a8e
mymacbook <1017962047@qq.com>
2018-11-07 11:46:59 +0800

    会员自助升级mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -5,11 +5,51 @@
  * Date: 2018/11/4
  * Time: 下午11:38
  */
+
+class  CreateCollection  {
+    protected  $cmd  = array();
+
+    function  __construct ( $collectionName ) {
+        $this -> cmd [ "create" ] = (string) $collectionName ;
+    }
+    function  setAutoIndexId ( $bool ) {
+        $this -> cmd [ "autoIndexId" ] = (bool) $bool ;
+    }
+    function  setCappedCollection ( $maxBytes ,  $maxDocuments  =  false ) {
+        $this -> cmd [ "capped" ] =  true ;
+        $this -> cmd [ "size" ]   = (int) $maxBytes ;
+
+        if ( $maxDocuments ) {
+            $this -> cmd [ "max" ] = (int) $maxDocuments ;
+        }
+    }
+    function  usePowerOf2Sizes ( $bool ) {
+        if ( $bool ) {
+            $this -> cmd [ "flags" ] =  1 ;
+        } else {
+            $this -> cmd [ "flags" ] =  0 ;
+        }
+    }
+    function  setFlags ( $flags ) {
+        $this -> cmd [ "flags" ] = (int) $flags ;
+    }
+    function  getCommand () {
+        return new  MongoDB\Driver\Command( $this -> cmd );
+    }
+    function  getCollectionName () {
+        return  $this -> cmd [ "create" ];
+    }
+}
+
+
 $m = new MongoDB\Driver\Manager("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
 
-$command = new \MongoDB\Driver\Command(['count' => '_id','query'=>'_id']);
-$result = $m->executeCommand('porn',$command);
-print_r($result);
+
+$command  =  $createCollection -> getCommand ();
+$cursor  =  $m-> executeCommand ( "porn" ,  $command );
+$response  =  $cursor -> toArray ()[ 0 ];
+var_dump ( $response );
+
 
 //$filter  = [];
 //$options = [
