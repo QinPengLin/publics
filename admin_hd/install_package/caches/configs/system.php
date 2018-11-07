c654bc88dadc32ef5407e3158f20d885806087dd
mymacbook <1017962047@qq.com>
2018-11-07 19:46:26 +0800

    会员自助升级mong

--- admin_hd/install_package/caches/configs/system.php
+++ admin_hd/install_package/caches/configs/system.php
@@ -42,7 +42,7 @@ return array(
 'admin_founders' => '1', //网站创始人ID，多个ID逗号分隔
 'execution_sql' => 0, //EXECUTION_SQL
 
-'phpsso' => '1',	//是否使用phpsso
+'phpsso' => '0',	//是否使用phpsso
 'phpsso_appid' => '1',	//应用id	
 'phpsso_api_url' => 'http://hd.qinpl.cn/phpsso_server',	//接口地址
 'phpsso_auth_key' => 'F8UT9xYothIu9xvkMPR5aCanpnM259xg', //加密密钥
