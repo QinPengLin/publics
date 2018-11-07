9f7b7c07b35d02eace0c4f1ee919c1878e1e4cf6
mymacbook <1017962047@qq.com>
2018-11-07 11:05:46 +0800

    会员自助升级mong

--- admin_hd/install_package/api/mongo.php
+++ admin_hd/install_package/api/mongo.php
@@ -7,13 +7,17 @@
  */
 $m = new MongoDB\Driver\Manager("mongodb://mongouser:Asdfgh123456@149.28.122.121:27017");
 
-$filter  = [];
-$options = [
-    'limit' => 10
-];
-$query   = new \MongoDB\Driver\Query($filter, $options);
-$rows    = $m->executeQuery('porn.porns', $query)->toArray();
-echo '<pre>';
-print_r($rows);
-echo '</pre>';
-exit;
+$command = new \MongoDB\Driver\Command(['count' => $collection,'query'=>$filter]);
+$result = $m->executeCommand('porn.porns',$command);
+print_r($result);
+
+//$filter  = [];
+//$options = [
+//    'limit' => 10
+//];
+//$query   = new \MongoDB\Driver\Query($filter, $options);
+//$rows    = $m->executeQuery('porn.porns', $query)->toArray();
+//echo '<pre>';
+//print_r($rows);
+//echo '</pre>';
+//exit;
