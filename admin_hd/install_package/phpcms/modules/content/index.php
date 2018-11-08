9b255dff2eece1152368c9cd663a9a93de4a8e3c
mymacbook <1017962047@qq.com>
2018-11-08 13:10:47 +0800

    会员自助升级mong

--- admin_hd/install_package/phpcms/modules/content/index.php
+++ admin_hd/install_package/phpcms/modules/content/index.php
@@ -271,10 +271,10 @@ class index {
             $p=[
                 'pageUrl' => ['$in' => [new \MongoDB\BSON\Regex('^.*?'.$zf.'.*?$','i')]]
             ];
-            $data = $mongodb->dpage($p,$page,16,['createTime'=>-1]);
+            $data = $mongodb->page($p,$page,16,['createTime'=>-1]);
             print_r($data);
             exit();
-            $data = $mongodb->dpage([],$page,16,['createTime'=>-1]);
+            $data = $mongodb->page([],$page,16,['createTime'=>-1]);
 
             $data_v=array();
             $i=0;
