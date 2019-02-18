4473c6b316cc312d39bbe29af3a747806d8f29ae
mymacbook <1017962047@qq.com>
2019-02-18 13:45:42 +0800

    swoole

--- bet365/swoole_server.php
+++ bet365/swoole_server.php
@@ -5,3 +5,16 @@
  * Date: 2019/2/18
  * Time: 下午1:34
  */
+$serv=new swoole_server('127.0.0.1',9501);
+$serv->on('connect',function (){
+    echo "Client: Connect.\n";
+});
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+    $serv->send($fd, "Server: ".$data);
+});
+$serv->on('close', function ($serv, $fd) {
+    echo "Client: Close.\n";
+});
+
+//启动服务器
+$serv->start();
